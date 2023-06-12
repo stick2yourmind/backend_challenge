15cb2806b56cbc93188d843cdef2951fe7768dad
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-06-12 16:22:49 -0300

    🔧 eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['typescript-sort-keys'],
+  rules: {
+    curly: ['warn', 'multi'],
+    'max-len': ['warn', {
+      code: 105,
+      ignoreComments: true,
+      ignoreRegExpLiterals: true,
+      ignoreTrailingComments: true
+    }],
+    quotes: ['error', 'single'],
+    'typescript-sort-keys/interface': 'error',
+    'typescript-sort-keys/string-enum': 'error'
+  }
+}
