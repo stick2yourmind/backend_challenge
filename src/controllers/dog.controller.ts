155331f3e141017ea88d831316060377e9b8d82f
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-06-12 20:57:31 -0300

    🔨 dog & ping controller created

---
+++ src/controllers/dog.controller.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express"
+import { HTTP_CODE } from "../data/httpCode.data"
+
+export const getAllDogs = async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const response = { message: "example"}
+    return res.status(HTTP_CODE.OK).json(response)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getDog = async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const response = { message: "example"}
+    return res.status(HTTP_CODE.OK).json(response)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const createDog = async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const response = { message: "example"}
+    return res.status(HTTP_CODE.OK).json(response)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const deleteDog = async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const response = { message: "example"}
+    return res.status(HTTP_CODE.OK).json(response)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const editDog = async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const response = { message: "example"}
+    return res.status(HTTP_CODE.OK).json(response)
+  } catch (error) {
+    next(error)
+  }
+}
---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express"
+import { HTTP_CODE } from "../data/httpCode.data"
+
+export const getPing = async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const response = { message: "example"}
+    console.log('🚀 ~ file: dog.controller.ts:10 ~ getAllDogs ~ response:', response)
+    return res.status(HTTP_CODE.OK).json(response)
+  } catch (error) {
+    next(error)
+  }
+}
