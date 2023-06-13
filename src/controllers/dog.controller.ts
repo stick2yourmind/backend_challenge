24bb687ead0b249872d31229e6ed96e484e5fedf
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-06-13 20:01:13 -0300

    ✨ validate request.body at /dog requests

--- src/controllers/dog.controller.ts
+++ src/controllers/dog.controller.ts
@@ -1,19 +1,20 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { HTTP_CODE } from '../data/httpCode.data'
+import { objectArrayToObjectSnake } from '../utils/adapters.util'
+import { GetDogValidate } from '../utils/validation/getDog.validate'
+import { CreateDogValidate } from '../utils/validation/createDog.validate'
 
 export const getAllDogs = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const response = { message: 'example' }
-    return res.status(HTTP_CODE.OK).json(response)
-  } catch (error) {
-    next(error)
-  }
-}
-
-export const getDog = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const response = { message: 'example' }
-    return res.status(HTTP_CODE.OK).json(response)
+    const { attribute, pageNumber, pageSize, order } = req.body
+    const dog = new GetDogValidate({ attribute, pageNumber, pageSize, order })
+    const isValid = await dog.isValid()
+    if (isValid?.value)
+      return res.status(HTTP_CODE.OK).json({ message: true })
+    else
+      return res
+        .status(HTTP_CODE.BAD_REQUEST)
+        .json({ error: isValid?.error })
   } catch (error) {
     next(error)
   }
@@ -21,26 +22,17 @@ export const getDog = async (req: Request, res: Response, next: NextFunction) =>
 
 export const createDog = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const response = { message: 'example' }
-    return res.status(HTTP_CODE.OK).json(response)
-  } catch (error) {
-    next(error)
-  }
-}
-
-export const deleteDog = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const response = { message: 'example' }
-    return res.status(HTTP_CODE.OK).json(response)
-  } catch (error) {
-    next(error)
-  }
-}
-
-export const editDog = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const response = { message: 'example' }
-    return res.status(HTTP_CODE.OK).json(response)
+    const { name, color, tail_length: tailLength, weight } = req.body
+    const dog = new CreateDogValidate({ name, color, tailLength, weight })
+    const isValid = await dog.isValid()
+    if (isValid?.value)
+      return res.status(HTTP_CODE.OK).json({ message: true })
+    else
+      return res
+        .status(HTTP_CODE.BAD_REQUEST)
+        .json({
+          error: objectArrayToObjectSnake(isValid?.error as Array<Record<string, string>>)
+        })
   } catch (error) {
     next(error)
   }
