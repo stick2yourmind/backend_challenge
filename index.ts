a4d5c2d24c4a799b096b3d77abe76b69794b9a08
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-06-14 01:18:35 -0300

    🔨 database model sync

--- index.ts
+++ index.ts
@@ -2,9 +2,13 @@ import server from './src/app'
 
 import { SERVER_PORT } from './src/config/server.config'
 import { errorLogger, infoLogger } from './src/config/logger.config'
+import { sequelize } from './src/database/database'
+import './src/models/color/color.model'
+import './src/models/dog/dog.model'
 
-function main () {
+async function main () {
   try {
+    await sequelize.sync({ alter: true })
     server.listen(SERVER_PORT, () => {
       infoLogger(`Server is up and running on port => ${SERVER_PORT}`)
     })
@@ -17,4 +21,5 @@ function main () {
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 main()
