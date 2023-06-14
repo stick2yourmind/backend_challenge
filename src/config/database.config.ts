f77640877b7b3ab345401a9f42d22b33e29b1d32
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-06-14 01:16:19 -0300

    🔧 database config constants

---
+++ src/config/database.config.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config'
+
+export const APP_DATABASE_NAME = process.env.APP_DATABASE_NAME ?? ''
+export const APP_DATABASE_USER = process.env.APP_DATABASE_USER ?? ''
+export const APP_DATABASE_PASSWORD = process.env.APP_DATABASE_PASSWORD ?? ''
+export const APP_DATABASE_HOST = process.env.APP_DATABASE_HOST ?? ''
