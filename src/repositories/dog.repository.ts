446d2542172ff38a4671212f1ea582e0dec73a75
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-06-16 03:24:26 -0300

    🔨 dog repository created

--- src/repositories/dog.repository.ts
+++ src/repositories/dog.repository.ts
@@ -1,45 +1,16 @@
+/* eslint-disable no-useless-catch */
 import { UniqueConstraintError } from '@sequelize/core'
 import { type IDog } from '../data/types.data'
 import Dog from '../models/dog.model'
 import CustomError from '../utils/error.util'
 import { HTTP_CODE } from '../data/httpCode.data'
-import Color from '../models/color.model'
-import '../models/associations.model'
+import { type IGetAllRepositoryProps } from './types.repository'
 
 export default class DogRepository {
   async create ({ name, color, tailLength, weight }: IDog): Promise<any> {
     try {
-      const [colorRes, created] = await Color.findOrCreate({
-        where: { name: color }
-      })
-      const [colorRes2, created2] = await Color.findOrCreate({
-        where: { name: 'blue' }
-      })
-      const newDog = await Dog.create(
-        { name, color, tail_length: tailLength, weight }
-      )
-      await newDog.addColor(colorRes)
-      await newDog.addColor(colorRes2)
-      const modifiedDog = await Dog.findByPk(
-        newDog.id,
-        {
-          include: [
-            {
-              model: Color,
-              as: 'colors',
-              attributes: ['name', 'id'],
-              through: {
-                attributes: []
-              }
-            }
-          ]
-        }
-      )
-      console.log('🚀 ~ file: dog.repository.ts:12 ~ DogRepository ~ create ~ created:', created)
-      console.log('🚀 ~ file: dog.repository.ts:14 ~ DogRepository ~ create ~ colorRes:', colorRes)
-      console.log('🚀 ~ file: dog.repository.ts:17 ~ DogRepository ~ create ~ modifiedDog:', modifiedDog)
-
-      return modifiedDog
+      const newDog = await Dog.create({ name, color, tail_length: tailLength, weight })
+      return newDog
     } catch (error) {
       if (error instanceof UniqueConstraintError) {
         let errorFields = ''
@@ -52,6 +23,20 @@ export default class DogRepository {
           statusCode: HTTP_CODE.BAD_REQUEST
         })
       }
+      throw error
+    }
+  }
+
+  async getAll ({ order, attribute, pageNumber, pageSize }: IGetAllRepositoryProps) {
+    try {
+      const dogs = await Dog.findAll({
+        attributes: ['id', 'name', 'weight', 'tail_length', 'color'],
+        order: [[attribute, order]],
+        offset: pageSize * (pageNumber - 1),
+        limit: pageSize
+      })
+      return dogs
+    } catch (error) {
       throw error
     }
   }
