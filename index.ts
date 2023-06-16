f96ea2250c6a7c86f957816c1f76afe7e9fc6121
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-06-16 03:29:58 -0300

    🔥 associations not required

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ import server from './src/app'
 
 import { SERVER_PORT } from './src/config/server.config'
 import { errorLogger, infoLogger } from './src/config/logger.config'
-import './src/models/associations.model'
+import { sequelize } from './src/database/database'
 
-server.listen(SERVER_PORT, () => {
+server.listen(SERVER_PORT, async () => {
+  await sequelize.sync()
   infoLogger(`Server is up and running on port => ${SERVER_PORT}`)
 })
 
